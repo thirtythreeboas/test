ccf434ff32c11c7d06bba88505de50e12d38f100
thirtythreeboas <tkdmeow@gmail.com>
2022-01-16 14:26:23 +0300

    beginner test

---
+++ script.js
@@ -0,0 +1,25 @@
+const disappear = () => {
+  if (button.style.display !== 'none') {
+    button.style.display = 'none';
+  } else {
+    button.style.display = 'flex';
+  }
+}
+
+window.onkeydown = disappear;
+
+let button = document.getElementById('button');
+
+const handleClick = param => {
+  const mouseOver = () => button.style.color = `${param !== undefined ? '#000' : '#f5f7f8'}`;
+  const mouseOut = () => button.style.color = `${param !== undefined ? '#fff' : '#5a5a6f'}`;
+  if (param === undefined) button.style.color = '#f5f7f8';
+  button.addEventListener('mouseover', mouseOver);
+  button.addEventListener('mouseout', mouseOut);
+};
+
+const reset = () => {
+    button.style.color = '#fff';
+    button.style.display = 'flex';
+    handleClick('reset');
+}
